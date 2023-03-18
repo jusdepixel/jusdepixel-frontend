57f1a86d1406ce3a39e875775aaa2c59f2ff9bb6
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:16:03 +0100

    ci: update inputs components

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,9 @@
 const Input = ({ disabled = false, className, ...props }) => (
-    <input disabled={disabled} className={`${className}`} {...props} />
+    <input
+        disabled={disabled}
+        className={`block mt-1 w-full ${className}`}
+        {...props}
+    />
 )
 
 export default Input
