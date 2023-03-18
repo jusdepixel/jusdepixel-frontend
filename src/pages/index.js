3c4829828e798960b43a766e105639018aa7b98a
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:17:34 +0100

    ci: updade index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import axios from '@/lib/axios'
 import ApplicationLogo from '@/components/ApplicationLogo'
-import ThemeToggle from '@/components/ThemeToggle'
+import ThemeToggler from '@/components/ThemeToggler'
 import { useAuth } from '@/hooks/auth'
+import { Loading } from '@/components/Loading'
 
 export default function Home() {
     const [domains, setDomains] = useState([])
-    const [loading, setLoading] = useState('loading')
+    const [loading, setLoading] = useState('')
     const [isDefault, setDefault] = useState(true)
     const { user } = useAuth({
         middleware: 'guest',
@@ -16,9 +17,9 @@ export default function Home() {
 
     const loadApi = async () => {
         await axios
-            .get('/api')
+            .get('/api/domains')
             .then(res => {
-                setDomains(res.data)
+                setDomains(res.data.datas)
             })
             .catch(error => {
                 if (error.response.status !== 422) throw error
@@ -26,11 +27,9 @@ export default function Home() {
     }
 
     useEffect(() => {
-        if (loading === 'loading') {
+        if (loading === '') {
             loadApi().then(() => {
-                setTimeout(() => {
-                    setLoading('loaded')
-                }, 100)
+                setLoading('loaded')
             })
         }
     })
@@ -41,12 +40,14 @@ export default function Home() {
                 <title>Jusdepixel</title>
             </Head>
 
+            {loading === '' && <Loading />}
+
             <div
                 className={`relative flex flex-col items-top justify-center min-h-screen items-center ${loading}`}>
-                <ThemeToggle />
-                <ApplicationLogo className="block h-14 w-auto fill-current text-gray-600 index mt-5" />
+                <ThemeToggler />
+                <ApplicationLogo className="h-14 index" />
 
-                {domains ? (
+                {domains && (
                     <div>
                         <div className="choose-domain flex flex-col items-center">
                             <div className="video-domain">
@@ -59,32 +60,12 @@ export default function Home() {
                                         type="video/mp4"
                                     />
                                 </video>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-play-circle-fill"
-                                    viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-                                </svg>
                             </div>
                             <div className="choose-domain-text text-lg text-gray-500">
                                 Choose your domain
                             </div>
                             <div className="text-gray-500 chevron">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="currentColor"
-                                    className="bi bi-chevron-down"
-                                    viewBox="0 0 16 16">
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-                                    />
-                                </svg>
+                                <i className="bi bi-chevron-down" />
                             </div>
                         </div>
 
@@ -105,22 +86,15 @@ export default function Home() {
                                     className={`domain text-center ${
                                         domain.default && isDefault && 'default'
                                     }`}>
-                                    <span
-                                        className="domain-front text-gray-600 block mb-2"
-                                        dangerouslySetInnerHTML={{
-                                            __html: domain.svg,
-                                        }}
-                                    />
+                                    <span className="domain-front text-gray-600 block mb-2">
+                                        <i
+                                            className={`mb-2 bi bi-${domain.icon}`}
+                                        />
+                                    </span>
                                     {domain.secure === 1 && (
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="16"
-                                            height="16"
-                                            fill="currentColor"
-                                            className="domain-secure"
-                                            viewBox="0 0 16 16">
-                                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
-                                        </svg>
+                                        <i
+                                            className={`bi bi-lock-fill domain-secure`}
+                                        />
                                     )}
                                     <span className="domain-back text-gray-600">
                                         {domain.name}
@@ -129,8 +103,6 @@ export default function Home() {
                             ))}
                         </div>
                     </div>
-                ) : (
-                    <></>
                 )}
             </div>
         </div>
