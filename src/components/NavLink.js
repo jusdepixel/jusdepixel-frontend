46f2e76f612a478209456844d1b54b45d5492f6e
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:14:49 +0100

    ci: update layout admin

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,10 +3,8 @@ import Link from 'next/link'
 const NavLink = ({ active = false, children, ...props }) => (
     <Link
         {...props}
-        className={`text-lg inline-flex items-center px-1 pt-1 border-b-2 leading-5 focus:outline-none transition duration-150 ease-in-out ${
-            active
-                ? 'border-cyan-400 text-gray-900 focus:border-cyan-500'
-                : 'border-transparent text-gray-400 hover:text-gray-900 font-light hover:border-gray-400 focus:text-gray-700 focus:border-gray-400'
+        className={`nav-link inline-flex items-center px-1 pt-1 leading-5 focus:outline-none ${
+            active && 'active'
         }`}>
         {children}
     </Link>
