efc7842fcf2c7dffdd95d632ef2e1268d8b58eb0
Jusdepixel <mandy@jusdepixel.fr>
2023-03-19 22:43:14 +0100

    ci(me): update theme pages

---
+++ src/pages/test/index.js
@@ -0,0 +1,15 @@
+const Index = () => {
+    return (
+        <div className="flex container">
+            <div className="w-1/4 text-center">ZONE 1</div>
+            <div className="flex-auto text-center h-full">
+                <div className="bg-sky-500">
+                    <div className="game"></div>
+                </div>
+            </div>
+            <div className=" w-1/4 text-center">ZONE 3</div>
+        </div>
+    )
+}
+
+export default Index
